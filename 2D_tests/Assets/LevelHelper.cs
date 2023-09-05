5985589df7521ea37def327978f469f2fb2c66a5
Julien <xbjujuox@hotmail.fr>
2023-09-06 00:00:52 +0200

    Change LevelHelper behavior a bit

---
+++ 2D_tests/Assets/LevelHelper.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class LevelHelper : MonoBehaviour
+{
+    public TextMeshProUGUI mTextMeshPro;
+    
+    void Start()
+    {
+        if(GameControler.currentWorld == 0 && GameControler.currentLevel == 0)
+        {
+            mTextMeshPro.text = "Move with ZQSD and catch the stars to cleared the level.";
+        }
+        else if (GameControler.currentWorld == 0 && GameControler.currentLevel == 1)
+        {
+            mTextMeshPro.text = "You lose when the enemy touch your line.";
+        }
+        else if (GameControler.currentWorld == 0 && GameControler.currentLevel == 2)
+        {
+            mTextMeshPro.text = "You can stop by going to the opposite direction on border.";
+        }
+        else
+        {
+            gameObject.SetActive(false);
+            GetComponent<Image>().enabled = false;
+        }
+    }
+
+}
