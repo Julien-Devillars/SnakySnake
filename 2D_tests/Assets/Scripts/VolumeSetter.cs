637257116a2a421e35e1f4ab9297b39b0893c23a
Julien <xbjujuox@hotmail.fr>
2023-09-09 17:55:28 +0200

    Move scripts in the correct folder

--- 2D_tests/Assets/Scripts/VolumeSetter.cs
+++ 2D_tests/Assets/Scripts/VolumeSetter.cs
@@ -1,8 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.Rendering.Universal;
-using UnityEngine.Rendering;
 
 public class VolumeSetter : MonoBehaviour
 {
